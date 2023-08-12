c2a70cdffe6df1d8adc43383f1aeceb53a23e89e
Rahul Anand <44201543+RahulGo8u@users.noreply.github.com>
2023-08-13 03:03:23 +0400

    Update factorial.js

--- assignments/week1/medium/factorial.js
+++ assignments/week1/medium/factorial.js
@@ -11,8 +11,3 @@ console.log(calculateFactorial(5));  // Expected output: 120
 console.log(calculateFactorial(0));  // Expected output: 1
 console.log(calculateFactorial(8));  // Expected output: 40320
 
-
-// Hint:The factorial of a positive integer n is the product of all positive integers from 1 to n.
-// For example:
-// The factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
-// The factorial of 0 is defined as 1.
