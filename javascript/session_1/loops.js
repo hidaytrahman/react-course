d8e05d9804256c549ca19754bf574f2ddc5a9d6d
Hidayt Rahman <hidaytrahman@gmail.com>
2023-08-12 13:04:14 +0530

    loops basics

--- javascript/session_1/loops.js
+++ javascript/session_1/loops.js
@@ -1,1 +1,36 @@
 // loops
+
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// for (let i = 0; i < days.length; i++) {
+// 	console.log('Today is ' + days[i]);
+// }
+
+// days.forEach((elm) => {
+// 	console.log('Hello ', elm);
+// });
+
+// map()
+days.map((elm) => {
+	console.log(` Day is ${elm}`);
+});
+
+// live example
+
+// want this transformation for days array
+// [
+//     {
+//         id: 1,
+//         title: "Sunday"
+//     }
+// ]
+
+// lets transform it
+const modifiedDays = days.map((item, index) => {
+	return {
+		id: index + 1,
+		title: item,
+	};
+});
+
+console.log({ modifiedDays });
