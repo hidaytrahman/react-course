c49ad0ad8913c0fc551d9d006778b6d7762723d9
Hidayt Rahman <hidaytrahman@gmail.com>
2023-08-12 13:03:11 +0530

    basics of functions

--- javascript/session_1/functions.js
+++ javascript/session_1/functions.js
@@ -1,1 +1,17 @@
+sayNoTo();
+
 // functions
+
+// function declarations / regular function
+// Hoisting
+
+function sayNoTo() {
+	console.log('War');
+}
+
+sayYesTo();
+
+// function expressions
+let sayYesTo = function () {
+	console.log('Peace');
+};
