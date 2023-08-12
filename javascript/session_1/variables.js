359fda9470da86775b8a21da0a7a1c3445a6d510
Hidayt Rahman <hidaytrahman@gmail.com>
2023-08-12 13:02:41 +0530

    learn about scopes

--- javascript/session_1/variables.js
+++ javascript/session_1/variables.js
@@ -8,4 +8,26 @@
 // Object
 // Array
 
+// var name = 'hidy';
+// var age = 40;
+// var isNightMode = false;
+// name = 20;
+// console.log(name);
+
+// let and const
+const name = 'hidy';
+var age = 40;
+let isNightMode = false;
+// name = 20;
+console.log(name);
+
+console.log('Hi ', name);
+
 // Interpolations
+console.log(`Hi ${name}
+* hellow 
+* nice
+* way
+
+* hi
+`);
